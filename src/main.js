2cb8be9a12b285c2365f5d3d4b994d0020f4a9c1
Gustavo Garica <gustavo.ggdrn@gmail.com>
2020-11-04 20:50:33 -0300

    feat: install Semantic UI  and utils folder configuration

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import store from "./store";
+import SuiVue from 'semantic-ui-vue';
+import 'semantic-ui-css/semantic.min.css';
 
 Vue.config.productionTip = false
+Vue.use(SuiVue);
 
 new Vue({
   store,
---
+++ src/utils/giphy-instance.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+export const api = axios.create({
+	baseURL: `https://api.giphy.com/v1/gifs`
+});
+
+const onError = ({ message, stack }) => {
+	console.error({ message, stack });
+	return { data: false };
+};
+
+api.interceptors.response.use(({ data }) => data, onError);
+export default api;
